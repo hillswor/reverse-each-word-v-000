07fb93f638709e88479456fa58d074f6d602e819
Bruce Hillsworth <bruce.hillsworth@gmail.com>
2018-09-24 17:10:58 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,6 @@
 def reverse_each_word(phrase)
-  reversed_phrase = []
-  phrase.split(" ").each do |word|
-    reversed_phrase << "#{word.reverse}"
+  reversed_phrase = phrase.split(" ").collect do |word|
+  word.reverse
   end
   reversed_phrase.join(" ")
 end
